ab3da5d8f06e1679055cb0aba84fa25a58369e86
ecflwhyte93 <ecflwhyte93@gmail.com>
2026-05-06 11:40:37 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-      },
-    ],
-  },
-}
+  reactStrictMode: true
+};
 
-export default nextConfig
+export default nextConfig;
